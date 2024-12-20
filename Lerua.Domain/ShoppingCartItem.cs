461a89f08962d3d124607b13e4764db5bc2ff60f
Domkr4t <ilia200266@gmail.com>
2024-12-20 13:04:55 +0300

    Initial Project and Implementation of the Domain layer
    Добавлены основные модели для слоя Domain корпоративной веб-системы "Интернет-магазин строительных материалов. Подбор товара".
    
    Список добавленных моделей:
    - Product: информация о товарах.
    - Category: категории товаров.
    - Customer: информация о клиентах.
    - Order: информация о заказах.
    - OrderItem: позиции заказов.
    - Supplier: информация о поставщиках.
    - User: данные пользователей системы.
    - ShoppingCart: структура корзины покупок.

---
+++ Lerua.Domain/ShoppingCartItem.cs
@@ -0,0 +1,8 @@
+﻿namespace Lerua.Domain
+{
+    public class ShoppingCartItem
+    {
+        public Guid ProductId { get; set; } // Ссылка на товар
+        public int Quantity { get; set; } // Количество
+    }
+}
