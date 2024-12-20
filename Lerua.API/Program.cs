4f638b8a77daf755ed453ade1eee49728460a055
Domkr4t <ilia200266@gmail.com>
2024-12-20 17:26:46 +0300

    Add Swagger
    Подключен Swagger для генерации и визуализации документации API:
    
    1. Добавлен пакет Swashbuckle.AspNetCore.
    2. Настроена регистрация Swagger в Program.cs.
    3. Добавлена поддержка XML-документации для расширенного описания API.
    
    Подготовлено для тестирования и дальнейшего документирования API.

--- Lerua.API/Program.cs
+++ Lerua.API/Program.cs
@@ -5,14 +5,32 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 
 builder.Services.AddPersistence(builder.Configuration);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+});
+
 var app = builder.Build();
-
-
 
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     app.UseHsts();
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Notes API V1");
+        c.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
